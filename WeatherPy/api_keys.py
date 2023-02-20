33c17f31d6d3db7f4f027a1e1bec41ca54a0fd6b
bjackson2022 <brandyn_jackson@colpal.com>
2023-02-20 18:50:36 -0500

    ading api keys

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "1f0e87e9cdea6849dcd501da0386aefd"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "prj_live_pk_b73d74e389e83e7e6b2d94fc5d31884326e25c77"
