170139577403ca1f0b241983f0f5452b87464fe8
bjackson2022 <brandyn_jackson@colpal.com>
2023-03-04 14:13:29 -0500

    final files

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -2,4 +2,4 @@
 weather_api_key = "1f0e87e9cdea6849dcd501da0386aefd"
 
 # Geoapify API Key
-geoapify_key = "prj_live_pk_b73d74e389e83e7e6b2d94fc5d31884326e25c77"
+geoapify_key = "453947ede4214c428034c0b25d7331f2"
